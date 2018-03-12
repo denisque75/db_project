a31f499546775794319a8cdab4dbf679328e7a51
Denis Telezhenko <denis_telezhenko@epam.com>
2018-03-12 19:00:16 +0200

    drawer was created

--- app/src/main/java/com/epam/denis_telezhenko/universityhelper/ui/main/MainActivity.java
+++ app/src/main/java/com/epam/denis_telezhenko/universityhelper/ui/main/MainActivity.java
@@ -1,5 +1,7 @@
 package com.epam.denis_telezhenko.universityhelper.ui.main;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
